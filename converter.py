3be169a9376b4a59d9ce4ac738921189129f8268
Denver James Duran <denverjames.duran19@outlook.com>
2023-03-28 11:37:22 +0800

    optimize runtime process by implementing thread pooling
    improve _gui.py file creation

--- converter.py
+++ converter.py
@@ -1,8 +1,11 @@
 import webbrowser, sys, subprocess, os
-from PySide2.QtWidgets import QFileDialog,QMainWindow,QMessageBox,QApplication
+from PySide2.QtWidgets import QFileDialog,QMainWindow,QMessageBox,QApplication,QToolTip
+from PySide2.QtGui import QCursor
 from main import Ui_MainWindow
+from threading import Thread
+import concurrent.futures
 
-class GuiProducts(QMainWindow):
+class Converter(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
@@ -13,6 +16,9 @@ class GuiProducts(QMainWindow):
         self.ui.convertbutton.clicked.connect(self.convert_ui_to_py)
         self.ui.gotogithub.clicked.connect(self.gotogithub)
         self.objecttypes=[    "QAction",    "QActionGroup",    "QApplication",    "QBoxLayout",    "QButtonGroup",    "QCalendarWidget",    "QCheckBox",    "QComboBox",    "QDateTimeEdit",    "QDial",    "QDialog",    "QDialogButtonBox",    "QDirModel",    "QDockWidget",    "QDoubleSpinBox",    "QErrorMessage",    "QFileIconProvider",    "QFileDialog",    "QFileOpenEvent",    "QFileSystemModel",    "QGraphicsScene",    "QGraphicsView",    "QGridLayout",    "QGroupBox",    "QHBoxLayout",    "QHeaderView",    "QInputDialog",    "QItemDelegate",    "QKeyEventTransition",    "QLCDNumber",    "QLabel",    "QLineEdit",    "QMainWindow",    "QMenu",    "QMenuBar",    "QMessageBox",    "QPushButton",    "QRadioButton",    "QRegExpValidator",    "QScrollBar",    "QShortcut",    "QSlider",    "QSpinBox",    "QSplashScreen",    "QSplitter",    "QStandardItemModel",    "QStatusBar",    "QStyledItemDelegate",    "QSyntaxHighlighter",    "QSystemTrayIcon",    "QTabBar",    "QTabWidget",    "QTableView",    "QTableWidget",    "QTableWidgetItem",    "QTextEdit",    "QTextBrowser",    "QToolBar",    "QToolButton",    "QToolTip",    "QTreeView",    "QTreeWidgetItem",    "QVBoxLayout",    "QGraphicsEllipseItem",    "QGraphicsItem",    "QGraphicsLineItem",    "QGraphicsPathItem",    "QGraphicsPixmapItem",    "QGraphicsPolygonItem",    "QGraphicsRectItem",    "QGraphicsSceneMouseEvent",    "QGraphicsSimpleTextItem",    "QGraphicsTextItem"]
+        self.overwrite_python_files=self.ui.option_overwritepython.isChecked()
+        self.create_gui_files=self.ui.option_guipython.isChecked()
+        self.create_txt_files=self.ui.option_txt.isChecked()
 
 
     def browse_for_compiler(self):  
@@ -32,7 +38,7 @@ class GuiProducts(QMainWindow):
             ui_file_paths = [path for path in file_paths if path.endswith(".ui")]
             # Set text of inputdirectory to comma-separated list of selected UI file paths
             self.ui.inputdirectory.setText(", ".join(ui_file_paths))
-            self.ui.outputdirectory.setText('/'.join([i for i in ui_file_paths[0].split('/')[:-1]]))
+            self.ui.outputdirectory.setText('/'.join([i for i in ui_file_paths[0].split('/')[:-1]]).lstrip())
 
     def browse_for_output(self):
         folder_dialog = QFileDialog(self)
@@ -42,35 +48,88 @@ class GuiProducts(QMainWindow):
             self.ui.outputdirectory.setText(folder_path)
     
     def convert_ui_to_py(self):
+        if self.ui.outputdirectory.text() == '' or self.ui.inputdirectory.text()=='':
+            QToolTip.showText(QCursor.pos(),"input/output directory shouldn't be empty")
+            return
         exe_path = self.ui.compilerdirectory.text()
         if exe_path =='':
             exe_path = f'PySide2-uic'
         input_directory = self.ui.inputdirectory.text()
         output_directory = self.ui.outputdirectory.text().split(',')[0].strip()
 
+
         # Check if any of the .py files have the same base name as the new .ui files
         input_directories = [os.path.dirname(d) for d in input_directory.split(',')]
+        if self.overwrite_python_files:
+            reply = QMessageBox.question(self, "Warning", f"The overwrite python files \
+checkbox is checked, this will overwrite ALL \
+files with the same name", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            try:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                    if reply == QMessageBox.No:
+                        try:
+                            for input_dir in input_directories:
+                                for ui_file_name in os.listdir(input_dir):
+                                    executor.submit(self.perform_operation_with_warning,input_dir,ui_file_name,output_directory,exe_path)
+                                    
+                            QMessageBox.information(self, "Success", "Conversion was successful!")
+                        except Exception as e:
+                                print(e)
+                                QMessageBox.information(self, f"Error", "Conversion was unsuccessful!\n\n{e}") 
+                    try:
+                        for input_dir in input_directories:
+                            for ui_file_name in os.listdir(input_dir):
+                                executor.submit(self.perform_operation,input_dir,ui_file_name,output_directory,exe_path)
+                        QMessageBox.information(self, "Success", "Conversion was successful!")
+                    except Exception as e:
+                            print(e)
+                            QMessageBox.information(self, f"Error", "Conversion was unsuccessful!\n\n{e}") 
+            except Exception as e:
+                print(e)
+                                
 
-        for input_dir in input_directories:
-            for ui_file_name in os.listdir(input_dir):
-                if ui_file_name.endswith(".ui"):
-                    ui_file_path = os.path.join(input_dir, ui_file_name)
-                    py_file_name = os.path.splitext(ui_file_name)[0] + ".py"
-                    py_file_path = os.path.join(output_directory, py_file_name)
-                    if os.path.exists(py_file_path):
-                        message = f"The file {py_file_name} already exists. Do you want to overwrite it?"
-                        reply = QMessageBox.question(self, "Warning", message, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-                        if reply == QMessageBox.No:
-                            continue
-                    self.convert_ui_file(exe_path, ui_file_path, py_file_path)
-                    self.process_py_file(py_file_path)
+
+    def perform_operation(self,input_dir,ui_file_name,output_directory,exe_path):
+
+        if ui_file_name.endswith(".ui"):
+            ui_file_path = os.path.join(input_dir, ui_file_name)
+            py_file_name = os.path.splitext(ui_file_name)[0] + ".py"
+            py_file_path = os.path.join(output_directory, py_file_name)
+            self.convert_ui_file(exe_path, ui_file_path, py_file_path)
+            py_file_path,class_name,names=self.get_python_details(py_file_path)   
+            
+            if self.create_gui_files:
+                self.create_gui_file(py_file_path,class_name)
+            if self.create_txt_files:
+                self.create_txt_file(py_file_path,names)
+    
+    def perform_operation_with_warning(self,input_dir,ui_file_name,output_directory,exe_path):
+        if ui_file_name.endswith(".ui"):
+            ui_file_path = os.path.join(input_dir, ui_file_name)
+            py_file_name = os.path.splitext(ui_file_name)[0] + ".py"
+            py_file_path = os.path.join(output_directory, py_file_name)
+            py_file_path,class_name,names=self.get_python_details(py_file_path)   
+            if os.path.exists(py_file_path):
+                message = f"The file {py_file_name} already exists. Do you want to overwrite it?"
+                reply = QMessageBox.question(self, "Warning", message, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+                if reply == QMessageBox.No:
+                    return
+                self.convert_ui_file(exe_path, ui_file_path, py_file_path)
+        if self.create_gui_files:
+                self.create_gui_file(py_file_path,class_name)
+        if self.create_txt_files:
+                self.create_txt_file(py_file_path,names)
+
+
+            
+
 
 
     def convert_ui_file(self, exe_path, ui_file_path, py_file_path):
         subprocess.run([exe_path, ui_file_path, "-o", py_file_path])
         return 
 
-    def process_py_file(self, py_file_path):
+    def get_python_details(self, py_file_path):
         names=[]
         with open(py_file_path, 'r') as py_file:
             class_name = ''
@@ -84,22 +143,24 @@ class GuiProducts(QMainWindow):
                         object_type = line[line.index('=')+1:].split()[0].split('(')[0]
                         if object_name in names:
                             continue
-                        names.append(f'self.ui.{object_name}  --> {object_type}')
-                    except Exception as e:
+                        names.append(f'{object_type}: self.ui.{object_name}')
+                    except:
                         continue
+        return py_file_path,class_name,sorted(names)
+    
+    def create_txt_file(self,py_file_path,names):
         try:
-            reply = QMessageBox.question(None, '', 'Would you like to add txt file containing all object names?', QMessageBox.No | QMessageBox.Yes, QMessageBox.No)
-            if reply == QMessageBox.Yes:
-                with open(py_file_path.replace('.py', '.txt'), 'w') as txt_file:
-                    for name in names:
-                        txt_file.write(f'{name}\n')
-            
-            reply = QMessageBox.question(None, '', 'Would you like to add _gui file?', QMessageBox.No | QMessageBox.Yes, QMessageBox.No)
-            if reply == QMessageBox.Yes:
-                file_name=py_file_path.replace('.','\\').split('\\')[-2]
-                new_file_name =py_file_path.replace('.py', '_gui.py')
-                with open(new_file_name, 'a') as gui_file:
-                    gui_file.write(f'from {file_name} import {class_name}\n\
+            with open(py_file_path.replace('.py', '.txt'), 'w') as txt_file:
+                for name in names:
+                    txt_file.write(f'{name}\n')
+        except Exception as e:
+            return e
+        
+    def create_gui_file(self,py_file_path,class_name):        
+        file_name=py_file_path.replace('.','\\').split('\\')[-2]
+        new_file_name =py_file_path.replace('.py', '_gui.py')
+        with open(new_file_name, 'a') as gui_file:
+                gui_file.write(f'from {file_name} import {class_name}\n\
 import sys \n\
 from PySide2.QtWidgets import QApplication,QMainWindow\n\n\
 class {file_name.title()}(QMainWindow):\n\
@@ -111,12 +172,10 @@ class {file_name.title()}(QMainWindow):\n\
 # Delete these if not needed\n\
 if __name__ == "__main__":\n\
     app = QApplication(sys.argv)\n\
-    window = {file_name}()\n\
+    window = {file_name.title()}()\n\
     window.show()\n\
     sys.exit(app.exec_())\n')
-            QMessageBox.information(self, "Success", "Conversion was successful!")
-        except Exception as e:
-            QMessageBox.information(self, f"Error", "Conversion was unsuccessful!\n\n{e}")
+
 
 
     def gotogithub(self):
@@ -125,7 +184,7 @@ if __name__ == "__main__":\n\
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = GuiProducts()
+    window = Converter()
     window.show()
     sys.exit(app.exec_())
 
--- main.py
+++ main.py
@@ -17,85 +17,273 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(304, 293)
-        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        MainWindow.resize(413, 382)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMinimumSize(QSize(304, 293))
-        MainWindow.setMaximumSize(QSize(304, 293))
+        MainWindow.setMaximumSize(QSize(3040, 2930))
         icon = QIcon()
         icon.addFile(u"converter.ico", QSize(), QIcon.Normal, QIcon.Off)
         MainWindow.setWindowIcon(icon)
-        MainWindow.setStyleSheet(u".QPushButton:hover{\n"
+        MainWindow.setStyleSheet(u".QPushButton:hover, .QCheckBox:hover{\n"
 "color: green;\n"
+"}\n"
+"\n"
+"\n"
+"QCheckBox:checked {\n"
+"color:red;\n"
 "}")
         MainWindow.setIconSize(QSize(32, 32))
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.compilerdirectory = QLineEdit(self.centralwidget)
-        self.compilerdirectory.setObjectName(u"compilerdirectory")
-        self.compilerdirectory.setGeometry(QRect(10, 30, 201, 20))
-        self.label = QLabel(self.centralwidget)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy1)
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.mainoptions = QFrame(self.centralwidget)
+        self.mainoptions.setObjectName(u"mainoptions")
+        sizePolicy1.setHeightForWidth(self.mainoptions.sizePolicy().hasHeightForWidth())
+        self.mainoptions.setSizePolicy(sizePolicy1)
+        self.mainoptions.setFrameShape(QFrame.StyledPanel)
+        self.mainoptions.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.mainoptions)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(3, 0, 0, 0)
+        self.label = QLabel(self.mainoptions)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 10, 201, 16))
-        self.compilerbrowse = QPushButton(self.centralwidget)
+
+        self.verticalLayout_2.addWidget(self.label)
+
+        self.frame_9 = QFrame(self.mainoptions)
+        self.frame_9.setObjectName(u"frame_9")
+        self.frame_9.setFrameShape(QFrame.StyledPanel)
+        self.frame_9.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_9)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.compilerdirectory = QLineEdit(self.frame_9)
+        self.compilerdirectory.setObjectName(u"compilerdirectory")
+
+        self.horizontalLayout_5.addWidget(self.compilerdirectory)
+
+        self.compilerbrowse = QPushButton(self.frame_9)
         self.compilerbrowse.setObjectName(u"compilerbrowse")
-        self.compilerbrowse.setGeometry(QRect(220, 30, 71, 23))
-        self.inputbrowse = QPushButton(self.centralwidget)
-        self.inputbrowse.setObjectName(u"inputbrowse")
-        self.inputbrowse.setGeometry(QRect(220, 80, 71, 23))
-        self.label_2 = QLabel(self.centralwidget)
+
+        self.horizontalLayout_5.addWidget(self.compilerbrowse)
+
+
+        self.verticalLayout_2.addWidget(self.frame_9)
+
+        self.label_2 = QLabel(self.mainoptions)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(10, 60, 201, 16))
-        self.inputdirectory = QLineEdit(self.centralwidget)
+
+        self.verticalLayout_2.addWidget(self.label_2)
+
+        self.frame_8 = QFrame(self.mainoptions)
+        self.frame_8.setObjectName(u"frame_8")
+        self.frame_8.setFrameShape(QFrame.StyledPanel)
+        self.frame_8.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_8)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.inputdirectory = QLineEdit(self.frame_8)
         self.inputdirectory.setObjectName(u"inputdirectory")
-        self.inputdirectory.setGeometry(QRect(10, 80, 201, 20))
-        self.label_3 = QLabel(self.centralwidget)
+
+        self.horizontalLayout_4.addWidget(self.inputdirectory)
+
+        self.inputbrowse = QPushButton(self.frame_8)
+        self.inputbrowse.setObjectName(u"inputbrowse")
+
+        self.horizontalLayout_4.addWidget(self.inputbrowse)
+
+
+        self.verticalLayout_2.addWidget(self.frame_8)
+
+        self.label_3 = QLabel(self.mainoptions)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(10, 110, 201, 16))
-        self.outputdirectory = QLineEdit(self.centralwidget)
+
+        self.verticalLayout_2.addWidget(self.label_3)
+
+        self.frame_7 = QFrame(self.mainoptions)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setFrameShape(QFrame.StyledPanel)
+        self.frame_7.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_7)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.outputdirectory = QLineEdit(self.frame_7)
         self.outputdirectory.setObjectName(u"outputdirectory")
-        self.outputdirectory.setGeometry(QRect(10, 130, 201, 20))
-        self.outputbrowse = QPushButton(self.centralwidget)
+
+        self.horizontalLayout_3.addWidget(self.outputdirectory)
+
+        self.outputbrowse = QPushButton(self.frame_7)
         self.outputbrowse.setObjectName(u"outputbrowse")
-        self.outputbrowse.setGeometry(QRect(220, 130, 71, 23))
-        self.convertbutton = QPushButton(self.centralwidget)
+
+        self.horizontalLayout_3.addWidget(self.outputbrowse)
+
+
+        self.verticalLayout_2.addWidget(self.frame_7)
+
+        self.frame_6 = QFrame(self.mainoptions)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_7 = QHBoxLayout(self.frame_6)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.frame_6)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.frame)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.option_overwritepython = QCheckBox(self.frame)
+        self.option_overwritepython.setObjectName(u"option_overwritepython")
+        self.option_overwritepython.setChecked(True)
+
+        self.verticalLayout.addWidget(self.option_overwritepython)
+
+        self.option_guipython = QCheckBox(self.frame)
+        self.option_guipython.setObjectName(u"option_guipython")
+        self.option_guipython.setChecked(True)
+
+        self.verticalLayout.addWidget(self.option_guipython)
+
+        self.option_txt = QCheckBox(self.frame)
+        self.option_txt.setObjectName(u"option_txt")
+        self.option_txt.setChecked(True)
+
+        self.verticalLayout.addWidget(self.option_txt)
+
+
+        self.horizontalLayout_7.addWidget(self.frame, 0, Qt.AlignTop)
+
+        self.frame_2 = QFrame(self.frame_6)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.label_5 = QLabel(self.frame_2)
+        self.label_5.setObjectName(u"label_5")
+        font = QFont()
+        font.setPointSize(8)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_5.setFont(font)
+
+        self.gridLayout.addWidget(self.label_5, 0, 0, 1, 1)
+
+        self.constructor_pushbutton = QCheckBox(self.frame_2)
+        self.constructor_pushbutton.setObjectName(u"constructor_pushbutton")
+        self.constructor_pushbutton.setChecked(True)
+
+        self.gridLayout.addWidget(self.constructor_pushbutton, 1, 0, 1, 1)
+
+        self.constructor_label = QCheckBox(self.frame_2)
+        self.constructor_label.setObjectName(u"constructor_label")
+
+        self.gridLayout.addWidget(self.constructor_label, 1, 1, 1, 1)
+
+        self.constructor_lineedit = QCheckBox(self.frame_2)
+        self.constructor_lineedit.setObjectName(u"constructor_lineedit")
+
+        self.gridLayout.addWidget(self.constructor_lineedit, 2, 0, 1, 1)
+
+
+        self.horizontalLayout_7.addWidget(self.frame_2)
+
+
+        self.verticalLayout_2.addWidget(self.frame_6)
+
+        self.frame_4 = QFrame(self.mainoptions)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.convertbutton = QPushButton(self.frame_4)
         self.convertbutton.setObjectName(u"convertbutton")
-        self.convertbutton.setGeometry(QRect(10, 160, 101, 23))
-        self.gotogithub = QPushButton(self.centralwidget)
+
+        self.horizontalLayout_2.addWidget(self.convertbutton)
+
+        self.gotogithub = QPushButton(self.frame_4)
         self.gotogithub.setObjectName(u"gotogithub")
-        self.gotogithub.setGeometry(QRect(140, 160, 151, 23))
-        self.label_4 = QLabel(self.centralwidget)
+
+        self.horizontalLayout_2.addWidget(self.gotogithub)
+
+
+        self.verticalLayout_2.addWidget(self.frame_4)
+
+        self.frame_5 = QFrame(self.mainoptions)
+        self.frame_5.setObjectName(u"frame_5")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.frame_5.sizePolicy().hasHeightForWidth())
+        self.frame_5.setSizePolicy(sizePolicy2)
+        self.frame_5.setFrameShape(QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.label_4 = QLabel(self.frame_5)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(10, 180, 281, 71))
+        self.label_4.setGeometry(QRect(0, 0, 391, 141))
         self.label_4.setLineWidth(1)
-        self.label_4.setAlignment(Qt.AlignCenter)
+        self.label_4.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
         self.label_4.setWordWrap(True)
+
+        self.verticalLayout_2.addWidget(self.frame_5)
+
+
+        self.horizontalLayout.addWidget(self.mainoptions)
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.option_guipython.toggled.connect(self.frame_2.setVisible)
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"UI Compiling Assistant - DDuran19", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"UI compiler directory:", None))
 #if QT_CONFIG(tooltip)
         self.compilerdirectory.setToolTip(QCoreApplication.translate("MainWindow", u"will use PySide2-uic as default", None))
 #endif // QT_CONFIG(tooltip)
-        self.label.setText(QCoreApplication.translate("MainWindow", u"UI compiler directory:", None))
         self.compilerbrowse.setText(QCoreApplication.translate("MainWindow", u"Browse", None))
-        self.inputbrowse.setText(QCoreApplication.translate("MainWindow", u"Browse", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Input directory", None))
+        self.inputbrowse.setText(QCoreApplication.translate("MainWindow", u"Browse", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Output directory", None))
         self.outputbrowse.setText(QCoreApplication.translate("MainWindow", u"Browse", None))
+        self.option_overwritepython.setText(QCoreApplication.translate("MainWindow", u"Overwrite existing python file", None))
+        self.option_guipython.setText(QCoreApplication.translate("MainWindow", u"Create _gui.file (will append existing file)", None))
+        self.option_txt.setText(QCoreApplication.translate("MainWindow", u"Create .txt file (will overwrite existing file)", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Constructor", None))
+        self.constructor_pushbutton.setText(QCoreApplication.translate("MainWindow", u"Push Buttons", None))
+        self.constructor_label.setText(QCoreApplication.translate("MainWindow", u"Labels", None))
+        self.constructor_lineedit.setText(QCoreApplication.translate("MainWindow", u"Line Edits", None))
         self.convertbutton.setText(QCoreApplication.translate("MainWindow", u"Convert Files", None))
         self.gotogithub.setText(QCoreApplication.translate("MainWindow", u"Go to GITHUB open source", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"An open source small project to conveniently use Pyside2/PYQT to convert ui files to python files. Will now add a .txt file containing all object names for easier coding, as well as creating a new GUI python file. Developed by Denver James Duran", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">An open source small project to assist users on compiling ui files</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">     - use PySide/PyQT to compile ui files</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">     - create a .txt with object names</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">     - create a _gu"
+                        "i.py files with constructor</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Developed by Denver James Duran</p></body></html>", None))
     # retranslateUi
 
